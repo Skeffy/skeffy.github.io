53c26cc5047e06ad0ccce9f11e8eaa254f74f6f6
Noah Stevens <stevens_nas@yahoo.com>
2024-08-09 15:47:07 -0400

    Project data now pulled from store

--- src/components/SingleProject.vue
+++ src/components/SingleProject.vue
@@ -1,17 +1,19 @@
 <script>
-
+export default {
+  props: ["project"]
+}
 </script>
 
 <template>
   <div class="project">
     <div id="project-photo">
-      <img src="" alt="Project thumbnail">
+      <img src="../assets/" alt="Project thumbnail">
       <button>Go to Project</button>
     </div>
     <div id="project-info">
-      <h3>Test Project</h3>
-      <h4>Java | Spring | Javascript</h4>
-      <p>This is the description of the roject. It does a whole bunch of whacky, crazy shit.</p>
+      <h3>{{ project.name}}</h3>
+      <h4>{{ project.tech }}</h4>
+      <p>{{ project.desc }}</p>
     </div>
   </div>
 </template>
@@ -22,6 +24,7 @@
   grid-template-columns: 1fr 1fr;
   grid-template-areas:
   "photo info";
+  margin-bottom: 50px;
 }
 
 #project-photo {
