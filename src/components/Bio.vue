f175267e30184c35696d4ac6864b0b32cd57e51d
Noah Stevens <stevens_nas@yahoo.com>
2024-10-09 16:51:06 -0400

    Updated about me text

--- src/components/Bio.vue
+++ src/components/Bio.vue
@@ -6,8 +6,10 @@
   <div id="bio">
     <img id="headshot" src="../assets/headshot.webp" alt="">
     <div id="about">
-        <h2>About Me</h2>
-        <p>This is some shit I be doin'.</p>
+        <h2>About Me:</h2>
+        <p>I am a software developer who loves a good challenge. Some of the technologies I use include Java, JavaScript, Vue.js, Spring Boot, and PostgreSQL, but there are many more I am familiar with. With a passion for learning and a drive to add to my toolbelt, 
+          I am constantly experimenting with new languages and technologies. Outside of my love of programming, I enjoy playing tabletop games with friends, and learning everything I can about rockety and space travel.
+        </p>
     </div>
   </div>
 </template>
@@ -25,7 +27,7 @@
     grid-area: photo;
     width: 250px;
     justify-self: right;
-    margin-right: 10px;
+    margin-right: 20px;
 }
 
 #about {
@@ -33,5 +35,6 @@
     display: flex;
     flex-direction: column;
     margin-left: 10px;
+    line-height: 2;
 }
 </style>
