9e080f74da34e417eda94ff418546f65fad1c16c
Noah Stevens <stevens_nas@yahoo.com>
2024-10-09 16:52:35 -0400

    Updated flag guesser link and image

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,14 @@ export function createStore(currentStats) {
     return _createStore({
         state: {
             projects: [
-                {name: "Flag Guesser", desc: "", img: "logo.svg", tech: "Java | Spring Boot | Vue.js"},
-                {name: "Test Project2", desc: "This project is also widdeway a test", img: "logo.svg", tech: "Javascript | Vue.js"},
+                {name: "Flag Guesser",
+                    desc: "A Worlde-like daily game where you have to guess the country that a flag belongs to. A Java/Spring Boot backend handles serving the client country information from a PostgreSQL database while the Vue.js front end handles the game logic and tracking user stats.",
+                    img: "/src/assets/flag-guesser-thumbnail.png", tech: "Java | Spring Boot | Vue.js",
+                    link: "https://guesstheflag.netlify.app"},
+                {name: "Test Project2",
+                    desc: "This project is also widdeway a test",
+                    img: "/src/assets/wip.png",
+                    tech: "Javascript | Vue.js"},
             ]
         },
 
