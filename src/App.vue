a446ff29880782cfb951d5625afe3e66ddb72f92
Noah Stevens <stevens_nas@yahoo.com>
2024-10-24 19:08:05 -0400

    Added 2 lists for skills and software

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,33 @@
 import PageHeader from './components/PageHeader.vue';
 import Bio from './components/Bio.vue';
 import Projects from './components/Projects.vue';
+import List from './components/List.vue';
 </script>
 
 <template>
-  <div>
+  <div id="app">
     <PageHeader />
     <main>
       <Bio />
+      <div id="lists">
+        <List :list="this.$store.state.skills"/>
+        <List :list="this.$store.state.softwares"/>
+      </div>
       <Projects />
     </main>
   </div>
 </template>
 
 <style scoped>
-div {
+#app {
   display: flex;
   flex-direction: column;
+}
+
+#lists {
+  margin-top: 100px;
+  margin-bottom: 100px;
+  display: flex;
+  justify-content: space-evenly;
 }
 </style>
