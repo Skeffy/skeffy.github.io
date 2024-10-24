1c1d21fd435a570145c801df4fc91ef2d7df91e5
Noah Stevens <stevens_nas@yahoo.com>
2024-10-24 19:08:50 -0400

    Created list template to fill with sotre data

---
+++ src/components/List.vue
@@ -0,0 +1,42 @@
+<script>
+export default {
+    props: ["list"]
+}
+</script>
+
+<template>
+    <div :id="this.list.name" class="list">
+        <h2 class="name">{{ this.list.name }}:</h2>
+        <ul class="bulletlist">
+            <li v-for="item in this.list.list" :value="item" class="listitem">{{ item }}</li>
+        </ul>
+    </div>
+</template>
+
+<style>
+.list {
+    background-color: rgb(206, 149, 63);
+    padding: 25px;
+    display: flex;
+    flex-direction: column;
+    height: fit-content;
+    width: fit-content;
+    margin: 10px;
+    border-radius: 25px;
+    box-shadow: 5px 5px 5px black;
+}
+
+.bulletlist {
+    columns: 2;
+}
+
+.name {
+    margin-left: auto;
+    margin-right: auto;
+    margin-bottom: 10px;
+}
+
+.listitem {
+    padding-right: 10px;
+}
+</style>
