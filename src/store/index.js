45fb3e7dddaa3cce1eaa60087b5cbf42794ccf3f
Noah Stevens <stevens_nas@yahoo.com>
2024-10-24 19:09:04 -0400

    Added data for skills and sofwatres

--- src/store/index.js
+++ src/store/index.js
@@ -4,15 +4,30 @@ export function createStore(currentStats) {
     return _createStore({
         state: {
             projects: [
-                {name: "Flag Guesser",
+                {
+                    name: "Flag Guesser",
                     desc: "A Worlde-like daily game where you have to guess the country that a flag belongs to. A Java/Spring Boot backend handles serving the client country information from a PostgreSQL database while the Vue.js front end handles the game logic and tracking user stats.",
                     img: "/flag-guesser-thumbnail.png", tech: "Java | Spring Boot | Vue.js",
-                    link: "https://guesstheflag.netlify.app"},
-                {name: "Dungeons and Dragons Character Creator",
+                    link: "https://guesstheflag.netlify.app"
+                },
+                {
+                    name: "Dungeons and Dragons Character Creator",
                     desc: "This project is in very early development. Stay tuned!",
                     img: "/wip.png",
-                    tech: "Javascript | Vue.js"},
-            ]
+                    tech: "Javascript | Vue.js"
+                },
+            ],
+
+            skills: {
+                    name: "Skills",
+                    list: ["Java", "Spring Boot", "PostgreSQL", "javaScript", "Docker", "Vue.js", "HTML", "CSS", "RESTful API", "Table Design", "JDBC", "Agile", "Unit Testing", "SOLID", "Git"]
+            },
+
+            softwares: {
+                name: "Softwares/Tools",
+                list: ["IntelliJ", "Postman", "PgAdmin", "VS Code", "Docker", "Android Studio", "GitHub", "AWS", "Azure"]
+
+            },
         },
 
         mutations: {
