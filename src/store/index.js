a197915139174ad4f15a62e1018e0ba15e40e129
Noah Stevens <stevens_nas@yahoo.com>
2024-10-26 01:01:36 -0400

    Fixed spelling error

--- src/store/index.js
+++ src/store/index.js
@@ -6,7 +6,7 @@ export function createStore(currentStats) {
             projects: [
                 {
                     name: "Flag Guesser",
-                    desc: "A Worlde-like daily game where you have to guess the country that a flag belongs to. A Java/Spring Boot backend handles serving the client country information from a PostgreSQL database while the Vue.js front end handles the game logic and tracking user stats.",
+                    desc: "A Wordle-like daily game where you have to guess the country that a flag belongs to. A Java/Spring Boot backend handles serving the client country information from a PostgreSQL database while the Vue.js front end handles the game logic and tracking user stats.",
                     img: "/flag-guesser-thumbnail.png", tech: "Java | Spring Boot | Vue.js",
                     link: "https://guesstheflag.netlify.app"
                 },
