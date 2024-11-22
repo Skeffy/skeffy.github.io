c58e867037b23c0c44bfafdd56f5fe195c4c37a7
Noah Stevens <stevens_nas@yahoo.com>
2024-11-22 14:56:42 -0500

    Created education array for storing education / certification data

--- src/store/index.js
+++ src/store/index.js
@@ -7,14 +7,16 @@ export function createStore(currentStats) {
                 {
                     name: "Flag Guesser",
                     desc: "A Wordle-like daily game where you have to guess the country that a flag belongs to. A Java/Spring Boot backend handles serving the client country information from a PostgreSQL database while the Vue.js front end handles the game logic and tracking user stats.",
-                    img: "/flag-guesser-thumbnail.png", tech: "Java | Spring Boot | Vue.js",
+                    img: "/flag-guesser-thumbnail.png",
+                    tech: "Java | Spring Boot | Vue.js | Bootstrap",
                     link: "https://guesstheflag.netlify.app"
                 },
                 {
                     name: "TellerNet",
                     desc: "A Java based server application for handling common bank teller duties such as creating customer profiles, opening new accounts, and logging account transactions. Information is stored via a PostgreSQL database and manipulated through a front-end built in Vue.js.",
                     img: "/wip.png",
-                    tech: "Java | PostgreSQL | Vue.js"
+                    tech: "Java | Spring Boot | PostgreSQL | Vue.js",
+                    link: ""
                 },
             ],
 
@@ -28,6 +30,14 @@ export function createStore(currentStats) {
                 list: ["IntelliJ", "Postman", "PgAdmin", "VS Code", "Docker", "Android Studio", "GitHub", "AWS", "Azure"]
 
             },
+
+            education: [
+                {
+                    name: "Tech Elevator",
+                    desc: "",
+                    date: "May 2024"
+                },
+            ]
         },
 
         mutations: {
