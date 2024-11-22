a7d71f2b78b99435839cd926c920d8eeec23a585
Noah Stevens <stevens_nas@yahoo.com>
2024-11-22 15:32:20 -0500

    added module class. now creates instance of SingleEducation for each entry in store > education

--- src/components/Education.vue
+++ src/components/Education.vue
@@ -1,11 +1,17 @@
 <script>
+import SingleEducation from './SingleEducation.vue';
 
+export default {
+    components: {
+        SingleEducation,
+    }
+}
 </script>
 
 <template>
-    <div id="education">
+    <div id="education" class="module">
         <h2>Education / Certifications</h2>
-        
+        <SingleEducation v-for="education in this.$store.state.education" :education="education"/>
     </div>
 </template>
 
